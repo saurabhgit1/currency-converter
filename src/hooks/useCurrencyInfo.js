9ac0d7e3e9968e5f992d06c8c30f4a87d7a83d27
saurabhgeminigit1 <singhsaurabhkumar84@gmail.com>
2024-04-17 17:08:29 +0530

    logic added

---
+++ src/hooks/useCurrencyInfo.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfo(currency) {
+  const [data, setData] = useState({});
+
+  useEffect(
+    () => {
+      fetch(
+        `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+      )
+        .then((curr) => curr.json())
+        .then((curr) => setData(curr[currency]));
+    },
+    [currency]
+  );
+  return data;
+}
+
+// function useCurrencyInfo(currency) {
+//   const [data, setData] = useState({});
+//   useEffect(() => {
+//     fetch(
+//       `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`
+//     )
+//       .then((res) => res.json())
+//       .then((res) => setData(res[currency]));
+//     console.log(data);
+//   }, [currency]);
+//   console.log(data);
+//   return data;
+// }
+
+export default useCurrencyInfo;
